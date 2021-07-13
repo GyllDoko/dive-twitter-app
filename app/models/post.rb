63991c4f89e3aaeeece631b4c29280b147553dbe
DOKO ALFA Gyll-christ <dokogyll@gmail.com>
2021-07-13 17:28:17 +0100

    correction

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,4 @@
 class Post < ApplicationRecord
-    has_one_attached :post_image
-    validates :title, presence: true, length: {maximum: 140, minimun:1}
     validates :description, presence: true, length: {maximum: 140, minimun:1}
-    
     
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_12_102232) do
+ActiveRecord::Schema.define(version: 2021_07_13_162102) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,7 +37,6 @@ ActiveRecord::Schema.define(version: 2021_07_12_102232) do
   end
 
   create_table "posts", force: :cascade do |t|
-    t.string "title"
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
