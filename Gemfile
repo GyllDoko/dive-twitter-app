394822c153d1482c78fc1aa68e17c8b0005ce7c7
DOKO ALFA Gyll-christ <dokogyll@gmail.com>
2021-07-13 21:58:01 +0100

    new correction on confirm page

--- Gemfile
+++ Gemfile
@@ -14,7 +14,7 @@ gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.5'
 
 
-
+gem 'bcrypt'
 gem 'bootsnap', '>= 1.1.0', require: false
 
 group :development, :test do
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,40 +1,33 @@
 class PostsController < ApplicationController
   before_action :set_post, only: %i[ show edit update destroy ]
 
-  # GET /posts or /posts.json
   def index
     @posts = Post.all
   end
 
-  # GET /posts/1 or /posts/1.json
   def show
   end
 
-  # GET /posts/new
   def new
     @post = Post.new
   end
 
-  # GET /posts/1/edit
   def edit
   end
 
-  # POST /posts or /posts.json
   def create
     @post = Post.new(post_params)
-
-    respond_to do |format|
-      if @post.save
-        format.html { redirect_to @post, notice: "Post was successfully created." }
-        format.json { render :show, status: :created, location: @post }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
-      end
+    if params[:back]
+      render :new
+    elsif @post.save
+      redirect_to posts_path, notice: "Post was successfully created." 
+    else
+      format.html { render :new, status: :unprocessable_entity }
+      format.json { render json: @post.errors, status: :unprocessable_entity }
     end
+    
   end
 
-  # PATCH/PUT /posts/1 or /posts/1.json
   def update
     respond_to do |format|
       if @post.update(post_params)
@@ -47,7 +40,6 @@ class PostsController < ApplicationController
     end
   end
 
-  # DELETE /posts/1 or /posts/1.json
   def destroy
     @post.destroy
     respond_to do |format|
@@ -55,14 +47,16 @@ class PostsController < ApplicationController
       format.json { head :no_content }
     end
   end
+  def confirm 
+    @post =Post.new(post_params)
+    render :new if @post.invalid?
+  end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
   def set_post
     @post = Post.find(params[:id])
   end
 
-  # Only allow a list of trusted parameters through.
   def post_params
     params.require(:post).permit( :description)
   end
